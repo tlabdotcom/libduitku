352fd78be64ddb33710c274e987e52547394485e
jiharal <jihar.akakom14@gmail.com>
2020-05-13 15:33:55 +0700

    add file test

--- libduitku_test.go
+++ libduitku_test.go
@@ -1,14 +1,18 @@
 package libduitku
 
-import "testing"
+import (
+	"fmt"
+	"testing"
+	"time"
+)
 
 func initClient() CoreDuitku {
 	c := NewClient()
-	c.Host = "https://sandbox.duitku.com",
-	c.MerchantCode = ""
-	c.APIKey = ""
-	c.ReturnURL = ""
-	c.CallbackURL = ""
+	c.Host = "https://sandbox.duitku.com"
+	c.MerchantCode = "code"
+	c.APIKey = "api-key"
+	c.ReturnURL = "https://yourweb.com/return-url"
+	c.CallbackURL = "https://yourweb.com/callback"
 	return CoreDuitku{
 		Client: c,
 	}
@@ -16,4 +20,34 @@ func initClient() CoreDuitku {
 
 func TestRequestTransaction(t *testing.T) {
 	c := initClient()
+	resData := DuitkuTransactionRequest{
+		PaymentMethod:   CreditCard,
+		PaymentAmount:   500000,
+		MerchantOrderID: fmt.Sprintf("KESAN%v", time.Now().Unix()),
+		ProductDetails:  "Transaksi pembayaran kesan",
+		Email:           "jihar@tlab.co.id",
+		CustomerVAName:  "Jihar Al Gifari",
+		PhoneNumber:     "08992939929",
+		ItemDetails: []DuitkuItemDetail{
+			{
+				Name:     "Item 1",
+				Price:    500000,
+				Quantity: 1,
+			},
+		},
+	}
+	resp, err := c.RequestTransaction(resData)
+	if err != nil {
+		t.Fatal(err)
+	}
+	t.Log(resp)
+}
+
+func TestCheckTransaction(t *testing.T) {
+	c := initClient()
+	resp, err := c.CheckTransaction("KESAN1589352383")
+	if err != nil {
+		t.Fatal(err)
+	}
+	t.Log(resp)
 }
